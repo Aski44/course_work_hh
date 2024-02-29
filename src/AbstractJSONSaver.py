0184154fe0181dc486b7d6b23260f7c61e26a7f4
Sasha <poriadchenko@yandex.ru>
2024-02-29 22:27:03 +0300

    123

--- src/AbstractJSONSaver.py
+++ src/AbstractJSONSaver.py
@@ -1,12 +1,1 @@
-from abc import ABC, abstractmethod
-
-
-class AbstractJsonSaver(ABC):
-
-    @abstractmethod
-    def save_file(self, data: list):
-        pass
-
-    @abstractmethod
-    def read_file(self):
-        pass
+1

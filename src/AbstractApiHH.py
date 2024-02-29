22b93805fc0f31e1abc16a51c8887ffd05068c90
Sasha <poriadchenko@yandex.ru>
2024-02-29 22:26:26 +0300

    1

--- src/AbstractApiHH.py
+++ src/AbstractApiHH.py
@@ -1,12 +1,1 @@
-from abc import ABC, abstractmethod
-
-
-class AbstractGetApiHh(ABC):
-
-    @abstractmethod
-    def __repr__(self):
-        pass
-
-    @abstractmethod
-    def get_vacancy_from_api(self, name_vacancy):
-        pass
+1
